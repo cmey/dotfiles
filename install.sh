9b8e4cc9ba783ccd134f5fb5f6401d62a6b4ea8d
Christophe MEYER <christophe.meyer.pro@gmail.com>
2016-09-21 20:08:49 -0400

    Rename . files

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ mkdir -p ~/bin
 brew install bash-completion
 brew install bash-git-prompt
 
-ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.bash_profile ~/.bash_profile
+ln -s $PWD/bashrc ~/.bashrc
+ln -s $PWD/bash_profile ~/.bash_profile
