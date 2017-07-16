4be7278d95de7ab546b2be569a59aaba58288e61
Christophe Meyer <cmeyer@butterflynetinc.com>
2017-07-15 21:48:39 -0400

    Fix GR.jl in .juliarc

--- juliarc.jl
+++ juliarc.jl
@@ -1,4 +1,12 @@
+# auto reload edited files
 @schedule begin
     sleep(0.1)
     @eval using Revise
+end
+
+# fix GR.jl: "Launching GKSTerm failed."
+@schedule begin
+    sleep(0.1)
+    @eval gkstermpath = joinpath(Pkg.dir("GR"), "deps/gr/Applications/GKSTerm.app")
+    @eval run(`open $gkstermpath`)
 end
