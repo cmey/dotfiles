d231919a9b5f0defdcf85c79c591ca84bd3e8979
Christophe Meyer <christophe.meyer.pro@gmail.com>
2018-09-21 10:18:46 -0400

    [Bfly] Update env-butterfly.sh to new folder archi

--- bin/bin/env-butterfly.sh
+++ bin/bin/env-butterfly.sh
@@ -1,4 +1,5 @@
-export BNI_DEV_ROOT=/Users/cmey/Code/ButterflyNetwork/software/host
+export BNI_REPO_ROOT=/Users/cmey/Code/ButterflyNetwork/software
+export BNI_DEV_ROOT=$BNI_REPO_ROOT/host
 export BNI_BUILD=$BNI_DEV_ROOT/Build
 export BNI_ROOT=$BNI_DEV_ROOT/build-release
-export PYTHONPATH=$BNI_DEV_ROOT/bazel-bin/Applications/BniHardwareWrapper:$BNI_ROOT/SDK/bin/release/osx64
+export PYTHONPATH=$BNI_REPO_ROOT/bazel-bin/host/Applications/BniHardwareWrapper
