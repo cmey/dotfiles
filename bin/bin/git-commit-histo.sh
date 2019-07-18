bdebc7b1bf8af26e25100fc48090f2ce77ce2dae
Christophe Meyer <christophe.meyer.pro@gmail.com>
2019-07-18 09:42:27 -0400

    [git] Add bin script to show hour graph of commits

---
+++ bin/bin/git-commit-histo.sh
@@ -0,0 +1,8 @@
+#!/usr/local/bin/bash
+
+git log \
+  --author="$1" \
+  --format="%ad" \
+  --date="format:%H" \
+  | awk '{n=$1+0;if(H[n]++>max)max=H[n]}END{for(i=0;i<24;i++){printf"%02d -%5d ",i,H[i];for(n=0;n<H[i]/max*50;n++){printf "*"}print""}}'
+
