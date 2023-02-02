298ab91e0db4aa21e3103aa7e1ae52b539ccd985
Christophe <christophe.meyer.pro@gmail.com>
2023-02-02 15:01:49 +0100

    [git] Make initial git.sh setup script

---
+++ git.sh
@@ -0,0 +1,5 @@
+git config --global user.name Christophe Meyer
+git config --global user.email christophe.meyer.pro@gmail.com
+# keep in memory for a few minutes
+git config --global credential.helper cache
+git config --global --add --bool push.autoSetupRemote true
