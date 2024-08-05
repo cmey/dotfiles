6401e6e661511f358b770d9f2e04b2ab6feca0c7
Christophe Meyer <christophe.meyer@lumavision.com>
2024-08-05 22:18:21 +0200

    Win: install NeoVim

--- install-windows.bat
+++ install-windows.bat
@@ -2,3 +2,4 @@ winget install "RipGrep MSVC"  # rg
 winget install Meld.Meld  # Meld (diff tool)
 winget install JanDeDobbeleer.OhMyPosh -s winget
 # then follow https://ohmyposh.dev/docs/installation/fonts and https://ohmyposh.dev/docs/installation/prompt
+winget install Neovim.Neovim
