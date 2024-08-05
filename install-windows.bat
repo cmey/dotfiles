0ca6340550d9fdb85c185aadc361d5b61cde4997
Christophe Meyer <christophe.meyer@lumavision.com>
2024-08-05 22:04:10 +0200

    Win: install OhMyPosh

--- install-windows.bat
+++ install-windows.bat
@@ -1,2 +1,4 @@
 winget install "RipGrep MSVC"  # rg
 winget install Meld.Meld  # Meld (diff tool)
+winget install JanDeDobbeleer.OhMyPosh -s winget
+# then follow https://ohmyposh.dev/docs/installation/fonts and https://ohmyposh.dev/docs/installation/prompt
