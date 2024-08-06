b956cf307779043973b2414840bf0807114079c4
Christophe Meyer <christophe.meyer@lumavision.com>
2024-08-06 11:26:59 +0200

    Win: install fd

--- install-windows.bat
+++ install-windows.bat
@@ -1,5 +1,6 @@
 winget install "RipGrep MSVC"  # rg
-winget install Meld.Meld  # Meld (diff tool)
+winget install sharkdp.fd  # fd
+winget install Neovim.Neovim
+winget install Meld.Meld  # meld (diff tool)
 winget install JanDeDobbeleer.OhMyPosh -s winget
 # then follow https://ohmyposh.dev/docs/installation/fonts and https://ohmyposh.dev/docs/installation/prompt
-winget install Neovim.Neovim
