ebd6cbfba324c996a9bd01169f5e6f22f444c9ae
Christophe Meyer <christophe.meyer@lumavision.com>
2024-09-20 09:18:50 +0200

    Win: add gst alias (git status)

--- install-windows.bat
+++ install-windows.bat
@@ -4,3 +4,10 @@ winget install Neovim.Neovim
 winget install Meld.Meld  # meld (diff tool)
 winget install JanDeDobbeleer.OhMyPosh -s winget
 # then follow https://ohmyposh.dev/docs/installation/fonts and https://ohmyposh.dev/docs/installation/prompt
+
+# then write in C:\Users\ChristopheMeyer\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# oh-my-posh init pwsh  --config 'C:\Users\ChristopheMeyer\AppData\Local\Programs\oh-my-posh\themes\tonybaloney.omp.json' | Invoke-Expression
+# $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+# git aliases
+# function Get-GitStatus { & git status $args }
+# New-Alias -Name gst -Value Get-GitStatus
